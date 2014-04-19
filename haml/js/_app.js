1900d2535870144358bcc1cd4bda01f4ad9c86d1
h0n24 <h0n24.cz@gmail.com>
2014-04-19 05:23:47 +0200

    prototyp 0.3e
    slovíčka, refactoring a okomentování skriptu

--- haml/js/_app.js
+++ haml/js/_app.js
@@ -1,7 +1,8 @@
-
+// scollpsy (posouvání levého menu při scrollování)
 $('body').scrollspy({ target: '#nav-scrollpsy', offset: '-100' });
 $(this).scrollspy('refresh');
 
+// funkce, která zajišťuje pěkné posouvání při kliku na href s kotvou na id (#top kupříkladu)
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -17,94 +18,95 @@ $(function() {
   });
 });
 
+// tabulka s fixovanými headry
 $("table.table-floathead").floatThead();
 
-
-
+// načítání slovíček do local storage, pokud se pracuje lokálně, nebo bez přístupu na internet, funkce try odchytí chybu
 try {
-var url = "min/vocabulary-simple.min.json"
-$.ajax ({
-    url: url,
-    dataType: 'json',
-    success: function (data) {
-        console.log("Načten [json] "+url+" + uložen do localStorage");
-        //console.log(data);
-        localStorage.setItem('vocabulary', JSON.stringify(data));
-        var today = new Date();
-        localStorage.setItem('vocabulary-updated', JSON.stringify(today));
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-        console.warn("chyba v [jsonu]: "+url+", stav: "+textStatus+", chybu háže: "+errorThrown);
-    }
-});
-
-var vocabulary = JSON.parse(localStorage.getItem('vocabulary'));
-
-var category;
-
-var categories = [];
-$.each(vocabulary, function( key, val ) {
-  //console.log(key);
-  categories.push("<li><a href=\"#scroll\" data-category=\""+key+"\">"+key+"</a></li>");
-});
-$("#set-slovicka").html(categories);
-
-function getTableItems(obj,cat) {
-  var items = [];
-  $.each( vocabulary[cat], function( key, val ) {
-    //console.log(val);
-    val = val.toString().replace(",",", ");
-    items.push("<tr><th>"+key+"</th><td>"+val+"</td></tr>")    
+  var url = "min/vocabulary-simple.min.json"
+  $.ajax ({
+      url: url,
+      dataType: 'json',
+      success: function (data) {
+          console.log("Načten [json] "+url+" + uložen do localStorage");
+          localStorage.setItem('vocabulary', JSON.stringify(data));
+          var today = new Date();
+          localStorage.setItem('vocabulary-updated', JSON.stringify(today));
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+          console.warn("[json] "+url+" nebyl správně načten.");
+      }
   });
-  $(obj).html(items);
-  $(obj).prev("h2").html(cat);
-  $("#set-slovicka li.active").removeClass("active");
-  $("#set-slovicka a[data-category='"+cat+"']").parent().addClass("active");
+} catch(err) {
+  console.warn("Problém s načítáním jsonu. Pracujete na localhostu? Err_code: " + err);
 }
 
-getTableItems("#get-slovicka","abstract");
+// načítání slovíček z local storage, generování stránky slovicka.haml
+try {
+  var vocabulary = JSON.parse(localStorage.getItem('vocabulary'));
 
-$("#set-slovicka a").on("click", function () {
-  category = $(this).data("category");
-  //console.log(category);
-  getTableItems("#get-slovicka",category);
-  $('.affix').removeClass("affix").addClass("affix-top").removeAttr('style');
-});
+  var render_table_categories = [];
+  $.each(vocabulary, function(key, val) {
+    render_table_categories.push("<li><a href=\"#scroll\" data-category=\""+key+"\">"+key+"</a></li>");
+  });
+  $("#set-slovicka").html(render_table_categories);
 
-//console.log(items);
+  function getTableItems(obj,cat) {
+    var items = [];
+    $.each(vocabulary[cat], function(key, val) {
+      //console.log(val);
+      val = val.toString().replace(",",", ");
+      items.push("<tr><th>"+key+"</th><td>"+val+"</td></tr>")    
+    });
+    $(obj).html(items);
+    $(obj).prev("h2").html(cat);
+    $("#set-slovicka li.active").removeClass("active");
+    $("#set-slovicka a[data-category='"+cat+"']").parent().addClass("active");
+  }
 
+  getTableItems("#get-slovicka","abstract");
+
+  $("#set-slovicka a").on("click", function () {
+    var category = $(this).data("category");
+    getTableItems("#get-slovicka",category);
+    $('.affix').removeClass("affix").addClass("affix-top").removeAttr('style');
+  });
+
+} catch(err) {
+  console.warn("Problém s načítáním localStorage. " + err);
 }
-catch(err) {
-  console.warn("Problém s načítáním jsonu. Pracujete na localhostu?");
-}
+
+
+// testování slovíček (beta verze kódu)
 
 $('#learning #input_word').focus();
 
-var word = "zelená";
-
 $('#learning #input_word').on("keyup", function () {
-  var actual = $(this).val();
-  word = word.toLowerCase();
-  actual = actual.toLowerCase();
-  console.log(actual);
+  var actual = $(this).val().toLowerCase();
+  var word = ["zelený", "zelená", "zelené"];
+  
   if (actual == "") {
     $('#learning .form-group').removeClass('has-success').removeClass('has-error');
     $('#learning .form-control-feedback').removeClass('glyphicon-remove').removeClass('glyphicon-ok');
   }
   else {
-    if (word.indexOf(actual) >= 0) {
-      $('#learning .form-group').removeClass('has-error').addClass('has-success');
-      $('.form-control-feedback').removeClass('glyphicon-remove').addClass('glyphicon-ok');
-      if (word == actual) {
-        $('#learning .form-group').removeClass('has-success');
-        $("#input_word").attr('disabled', true);
-        $("#next_word").removeClass("btn-default").addClass("btn-primary");
-        $("#next_word").focus();
+    for (var i = 0; i < word.length; i++) {
+      word[i] = word[i].toLowerCase();
+      if (word[i].indexOf(actual) >= 0) {
+        $('#learning .form-group').removeClass('has-error').addClass('has-success');
+        $('.form-control-feedback').removeClass('glyphicon-remove').addClass('glyphicon-ok');
+        if (word[i] == actual) {
+          $('#learning .form-group').removeClass('has-success');
+          $("#input_word").attr('disabled', true);
+          $("#next_word").removeClass("btn-default").addClass("btn-primary");
+          $("#next_word").focus();
+          break;
+        }
       }
-    }
-    else {
-      $('#learning .form-group').removeClass('has-success').addClass('has-error');
-      $('.form-control-feedback').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+      else {
+        $('#learning .form-group').removeClass('has-success').addClass('has-error');
+        $('.form-control-feedback').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+      };
     };
   };
 
